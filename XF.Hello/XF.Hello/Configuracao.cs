1b7188da1cfe541837bcdf6f6ccec02251e189b8
Logon Aluno <logonrmlocal@fiap.com.br>
2018-10-10 22:38:13 -0300

    Add project files.

---
+++ XF.Hello/XF.Hello/Configuracao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XF.Hello
+{
+    public class Configuracao
+    {
+        public bool localizacao { get; set; }
+
+        public bool sms { get; set; }
+
+        public bool cookies { get; set; }
+
+        public bool mural { get; set; }
+
+        public bool email { get; set; }
+    }
+}
